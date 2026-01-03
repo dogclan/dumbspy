9a3409e12f0241812f35adbc173f087738986499
cetteup <me@cetteup.com>
2026-01-03 17:13:45 +0100

    fix: add missing gamespy packet tags to GamespyLoginRequest struct

--- internal/loginrequest.go
+++ internal/loginrequest.go
@@ -5,16 +5,16 @@ import (
 )
 
 type GamespyLoginRequest struct {
-	Login       *string `validate:"len=0,required"` // Key only, must be empty
-	Challenge   string  `validate:"len=32,required"`
-	UniqueNick  string  `validate:"min=1,required"`
-	Response    string  `validate:"md5,required"`
-	Port        string  `validate:"numeric,required"`
-	ProductID   string  `validate:"numeric,required"`
-	GameName    string  `validate:"min=1,required"`
-	NamespaceID string  `validate:"numeric,required"`
-	SDKRevision string  `validate:"numeric,required"`
-	ID          string  `validate:"numeric,required"`
+	Login       *string `gamespy:"login" validate:"len=0,required"` // Key only, must be empty
+	Challenge   string  `gamespy:"challenge" validate:"len=32,required"`
+	UniqueNick  string  `gamespy:"uniquenick" validate:"min=1,required"`
+	Response    string  `gamespy:"response" validate:"md5,required"`
+	Port        string  `gamespy:"port" validate:"numeric,required"`
+	ProductID   string  `gamespy:"productid" validate:"numeric,required"`
+	GameName    string  `gamespy:"gamename" validate:"min=1,required"`
+	NamespaceID string  `gamespy:"namespaceid" validate:"numeric,required"`
+	SDKRevision string  `gamespy:"sdkrevision" validate:"numeric,required"`
+	ID          string  `gamespy:"id" validate:"numeric,required"`
 }
 
 func (r GamespyLoginRequest) Validate() error {
